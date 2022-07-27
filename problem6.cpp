4aa5052734d790850a5de25c177a0c5af386c6cc
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-07-28 00:02:09 +0600

    Complete Problem Six

---
+++ problem6.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int m;
+    cin >> m;
+    int b[m];
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b[i];
+    }
+    bool found = false;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (a[i] == b[j])
+            {
+                cout << a[i] << " ";
+                found = true;
+            }
+        }
+    }
+    if (!found)
+    {
+        cout << "Empty set" << endl;
+    }
+    return 0;
+}
